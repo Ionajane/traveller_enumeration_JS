cc87dca6702182e7bf5d6b468915b4b351a23faf
ionawright <ionawright@live.com>
2018-10-17 16:29:10 +0100

    completed_journey_end_locations

---
+++ models/traveller.js
@@ -0,0 +1,31 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map(journey => journey.startLocation)
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map(journey => journey.endLocation)
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+// we want to filter the journey arrays and find the transport used.
+  return this.journeys.filter(transport => )
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
